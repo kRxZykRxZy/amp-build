b40a8d94c77b58341a9330e4ec78e52d1b184f8e
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-09-28 15:45:42 +0100

    Update index.sh

--- index.sh
+++ index.sh
@@ -4,12 +4,12 @@ sh deploy/aw3/build.sh
 pwd
 mkdir aw3/temp
 pwd
-cp -R /aw3/build aw3/temp
+cp -R aw3/build aw3/temp
 pwd
 sh deploy/ampmod/clone.sh
 pwd
 sh deploy/ampmod/build.sh
 pwd
-cp -R /ampmod/packages/gui/build aw3/build
+cp -R ampmod/packages/gui/build aw3/build
 pwd
 cp -R aw3/temp aw3/build
